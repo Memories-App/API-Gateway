b258120e0217cee0a73a69b88da7a33f0d7b39a8
Jonas <JNSAPH@icloud.com>
2023-06-22 23:31:26 +0200

    Added ImageService to gateway

--- config/microservices.ts
+++ config/microservices.ts
@@ -1,7 +1,9 @@
 export const microservicesDev = {
     authentication: 'http://localhost:4000',
+    image: 'http://localhost:5000'
 };
 
 export const microservicesProd = {
     authentication: 'http://ms_authservice:4000',
+    image: 'http://ms_imageservice:5000'
 };
